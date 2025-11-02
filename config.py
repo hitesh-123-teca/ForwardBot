a21a64003f46ebf7c7de4211c06946c85c592f56
hiteshrathiya <hiteshrathiya123@gmail.com>
2025-11-02 08:13:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "28580773")
-    API_HASH = os.environ.get("API_HASH", "f80e465a8805bee5c6bf29fa12d6cc0c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7794943135:AAG3U8Cu9U3I4EZOGu4WA_o-oToQSHNmYnU") 
+    API_ID = os.environ.get("API_ID", "21688431")
+    API_HASH = os.environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8297730256:AAFOVC2WPyQO75IfSp6xjLLTRa3Jvt-S4JM") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://telegrambott19:telegrambott19@cluster0.hetez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://moviescorn:moviescorn@hitu.4jr5k.mongodb.net/?retryWrites=true&w=majority&appName=Hitu")
+    DB_NAME = os.environ.get("DB_NAME", "Hitu")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6861892595').split()]
 
 
 class temp(object): 
