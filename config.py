aa565c55ee785dd3b587ef0401c52e6b40c64b13
hiteshrathiya <hiteshrathiya123@gmail.com>
2026-02-21 07:46:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7852091851:AAE_FNIVvteZqSlrJerUVaNq13TWoOlDm0E") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://moviescorn:moviescorn@hitu.4jr5k.mongodb.net/?retryWrites=true&w=majority&appName=Hitu")
-    DB_NAME = os.environ.get("DB_NAME", "Hitu")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Forward:Forwardbot@cluster0.jo0tong.mongodb.net/?appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6861892595').split()]
 
 
