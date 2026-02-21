e50f7b20648c371294973a4a435326ec9f2ae9df
hiteshrathiya <hiteshrathiya123@gmail.com>
2026-02-21 06:47:22 +0530

    Update BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "21688431")
     API_HASH = os.environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8297730256:AAFOVC2WPyQO75IfSp6xjLLTRa3Jvt-S4JM") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7852091851:AAE_FNIVvteZqSlrJerUVaNq13TWoOlDm0E") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://moviescorn:moviescorn@hitu.4jr5k.mongodb.net/?retryWrites=true&w=majority&appName=Hitu")
     DB_NAME = os.environ.get("DB_NAME", "Hitu")
