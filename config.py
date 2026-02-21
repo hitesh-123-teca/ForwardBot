1619ea0a447960af2cf452020a50907c1accec53
hiteshrathiya <hiteshrathiya123@gmail.com>
2026-02-21 08:42:06 +0530

    Update BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "21688431")
     API_HASH = os.environ.get("API_HASH", "db274cb8e9167e731d9c8305197badeb")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7852091851:AAE_FNIVvteZqSlrJerUVaNq13TWoOlDm0E") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7852091851:AAFBJjNRPlFf8LjtEkgybnuCQAWGqGwx51U") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://Forward:Forwardbot@cluster0.jo0tong.mongodb.net/?appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "Cluster0")
